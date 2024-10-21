d5e6ed9ba900e0bfbe55189ba1d264b88b7d1187
Guglielmo Barison <guglielmobarison00@gmail.com>
2024-10-21 16:40:27 +0200

    Create index.php | continue

--- index.php
+++ index.php
@@ -6,11 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello World</title>
 </head>
-
 <body>
     <h1><?php echo "Hello, World!"; ?></h1>
 </body>
-
 </html>
 
 
